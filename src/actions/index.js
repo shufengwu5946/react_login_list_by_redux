ed5061f2484ddbf8eeb52bf58c90ec005a4fc94b
shufengwu5946 <594695686@qq.com>
2019-01-07 01:48:10 +0800

    redux-actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,87 +2,43 @@ import {
     fetchLogin
 } from '../utils';
 import getList from '../other/get-list';
+import { createActions } from 'redux-actions';
 
-// 增加todo列表项
-export const addTodo = (title, userId) => {
-    return {
-        type: 'ADD_TODO',
-        title,
-        userId
-    }
-}
+export const { 
+    addTodo,
+    deleteTodo,
+    loadTodo,
+    logOutSuccess,
+    logInSuccess,
+    logInStart,
+    logInFail,
+    setUserName,
+    setPassword,
+    setLoginLoading
+} = createActions({
+    // 增加todo列表项
+    ADD_TODO: (title ,userId) => ({ title,userId}),
+    // 删除todo列表项
+    DELETE_TODO:(id)=>({id}),
+    // 加载todo列表成功
+    LOAD_TODO:(todos)=>({todos}),
+    // 登出失败
+    LOG_OUT_SUCCESS:()=>({}),
+    // 登录成功
+    LOG_IN_SUCCESS:(userName,userId)=>({userName,userId}),
+    // 登录开始
+    LOG_IN:()=>({}),
+    // 登录失败
+    LOG_IN_FAIL:(loginInfo)=>({loginInfo}),
+    // 设置用户名称
+    SET_USER_NAME:(userName)=>({userName}),
+    // 设置密码
+    SET_PASSWORD:(password)=>({password}),
+    // 设置登录loading
+    SET_LOGIN_LOADING:(loading)=>({loading})
 
-// 删除todo列表项
-export const deleteTodo = (id) => {
-    return {
-        type: 'DELETE_TODO',
-        id
-    }
-}
 
-// 加载todo列表成功
-export const loadTodo = (todos) => {
-    return {
-        type: 'LOAD_TODO',
-        todos
-    }
-}
-
-// 登出失败
-export const logOutSuccess = () => {
-    return {
-        type: 'LOG_OUT_SUCCESS'
-    }
-}
-
-// 登录成功
-export const logInSuccess = (userName, userId) => {
-    return {
-        type: 'LOG_IN_SUCCESS',
-        userName,
-        userId
-    }
-}
-
-// 登录开始
-export const logInStart = () => {
-    return {
-        type: 'LOG_IN'
-    }
-}
-
-// 登录失败
-export const logInFail = (loginInfo) => {
-    return {
-        type: 'LOG_IN_FAIL',
-        loginInfo
-    }
-}
-
-// 设置用户名称
-export const setUserName = (userName) => {
-    return {
-        type: 'SET_USER_NAME',
-        userName
-    }
-
-}
-
-// 设置密码
-export const setPassword = (password) => {
-    return {
-        type: 'SET_PASSWORD',
-        password
-    }
-}
-
-// 设置登录loading
-export const setLoginLoading = (loading) => {
-    return {
-        type: 'SET_LOGIN_LOADING',
-        loading
-    }
-}
+});
 
 // 登录，异步操作，采用react-thunk
 export const login = () => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,96 @@
-import { combineReducers } from 'redux'
-import {todos} from './todos'
-import {loginInfo,login,userName,userId,password,loading,progress} from './log';
+import { handleActions} from 'redux-actions';
+import {
+  getMaxId,
+  getCount
+} from '../other/other';
 
-const todoApp = combineReducers({
-  todos,
-  loginInfo,
-  login,
-  userName,
-  userId,
-  password,
-  loading,
-  progress
-})
+const defaultState = {
+  todos:[],
+  loginInfo:'',
+  login:false,
+  userName:'',
+  userId:0,
+  password:'',
+  loading:false,
+  progress:false
+};
+
+const todoApp = handleActions({
+  ADD_TODO:(state,action)=>({
+    ...state,
+    todos:[
+      {
+          id: (getMaxId(state.todos) + 1),
+          title: action.title,
+          completed: false,
+          userId: action.userId
+      }
+    ]
+  }),
+  DELETE_TODO:(state,action)=>({
+    ...state,
+    todos:state.todos.map(value =>
+      (value.id === action.id) ? { ...value,
+          completed: true
+      } : value
+    )
+  }),
+  LOAD_TODO:(state,action)=>({
+    ...state,
+    todos:[...action.todos],
+    progress:false
+  }),
+  LOG_OUT_SUCCESS:(state,action)=>({
+    ...state,
+    login:false,
+    userName:'',
+    password:'',
+    loading:false
+  }),
+  LOG_IN_SUCCESS:(state,action)=>({
+    ...state,
+    loginInfo:'',
+    login:true,
+    userName:action.userName,
+    userId:action.userId,
+    loading:false,
+    progress:true,
+    todos:[]
+  }),
+  LOG_IN:(state,action)=>({
+    ...state,
+    loginInfo:'',
+    loading:true
+  }),
+  LOG_IN_FAIL:(state,action)=>({
+    ...state,
+    loginInfo:action.loginInfo,
+    login:false,
+    loading:false,
+
+  }),
+  SET_USER_NAME:(state,action)=>({
+    ...state,
+    userName:action.userName
+  }),
+  SET_PASSWORD:(state,action)=>({
+    ...state,
+    password:action.password
+  }),
+  SET_LOGIN_LOADING:(state,action)=>({
+    ...state
+  })
+},defaultState);
+
+// const todoApp = combineReducers({
+//   todos,
+//   loginInfo,
+//   login,
+//   userName,
+//   userId,
+//   password,
+//   loading,
+//   progress
+// })
 
 export default todoApp
